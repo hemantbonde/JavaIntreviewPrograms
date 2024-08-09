e54fa4e6e14363c88d7be5d9c0f180adb9c02e11
hemantbonde <92773483+hemantbonde@users.noreply.github.com>
2024-08-10 00:14:09 +0530

    Update PracScenario.java
    Commited

--- java_program/src/Array_program/PracScenario.java
+++ java_program/src/Array_program/PracScenario.java
@@ -11,6 +11,21 @@ public class PracScenario {
 		int arr[]= {20,50,40,30,10,50,40,50};
 		Set<int[]> set = new HashSet<>(Arrays.asList(arr));
 		System.out.println(set);
+				int arr[] = { 20, 50, 40, 30, 10, 50, 40, 50 };
+
+		System.out.println(Arrays.toString(arr));
+		HashSet<Integer> set = new HashSet<Integer>();
+		for (int num : arr) {
+			set.add(num);
+		}
+		Integer[] arrInt=set.toArray(new Integer[0]);
+		Arrays.sort(arrInt);
+		int[] newArr=new int[arrInt.length];
+		for (int i=0; i<arrInt.length; i++) {
+			newArr[i]=arrInt[i];
+		}
+		System.out.println(Arrays.toString(newArr));
+
 		
 		
 
